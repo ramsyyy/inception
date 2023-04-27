cf14f9e8f6581074e2ad6596e11c5c781e2ab3b2
ramsyyy <aga.ramadann@gmail.com>
2023-04-27 19:52:06 +0200

    ff

--- srcs/requirements/mariadb/dbscript.sh
+++ srcs/requirements/mariadb/dbscript.sh
@@ -6,4 +6,11 @@ service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e 
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGESS;"
+
+mysqladmin -u root -pp${SQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
+
+echo "MariaDB database and user were created successfullu! "
