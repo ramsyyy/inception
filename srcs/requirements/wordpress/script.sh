7490bf4a3c38b2628005df73b038ae57521a970a
ramsyyy <aga.ramadann@gmail.com>
2023-05-18 20:17:21 +0200

    prresque

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,10 +1,33 @@
 sleep 10
 
+wp core download --allow-root --path="/var/www/wordpress"
+
+
 wp config create --allow-root \
 		--dbname=$DATABASE_NAME \
 		--dbuser=$USERNAME \
 		--dbpass=$PASSWORD \
-		--dbhost=mariadb:3306 --path='var/www/wordpress'
+		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+echo "jsui la "
+
+wp core install	--allow-root\
+			--admin_user="$ADMIN_USER"\
+			--admin_password="$ADMIN_PASS"\
+			--admin_email="$ADMIN_USER@example.com"\
+			--url="$DOMAIN_NAME"\
+			--title="Inception"\
+			--skip-email\
+			--path="/var/www/wordpress"
+
+echo "et la "
+
+wp user create	--allow-root\
+			"$WP_USER"\
+			"$WP_USER"@example.com\
+			--role=author\
+			--user_pass="$WP_PASS"\
+			--path="/var/www/wordpress"
 
 chmod -R 777 /var/www/wordpress
 
