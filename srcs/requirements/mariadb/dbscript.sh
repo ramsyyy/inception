726f4836deffd4a27b9c56ea6cdabd471478e198
ramsyyy <aga.ramadann@gmail.com>
2023-05-19 00:11:58 +0200

    qwe

--- srcs/requirements/mariadb/dbscript.sh
+++ srcs/requirements/mariadb/dbscript.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 
-mysqld
+mysqld &
+PID=$?
+sleep 1
+
 if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
     echo "Database already installed"
@@ -16,17 +19,22 @@ n
 y
 y
 _EOF_
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot
-echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+echo "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot
 echo "FLUSH PRIVILEGES;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
-echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
-echo "FLUSH PRIVILEGES;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress_db.sql
-echo "Database created"
+#echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"|  mysql -uroot -p$MYSQL_ROOT_PASSWORD
+#echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+#echo "FLUSH PRIVILEGES;" | mysql -uroot -p$MYSQL_ROOT_PASSWORD
+##mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress_db.sql
+#echo "Database created"
+#
+#fi
+#
+#mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 
-fi
+#echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -uroot
+#exec "$@"
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+kill -9 $PID
 
-exec "$@"
+exec mysqld
